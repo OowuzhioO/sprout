878ea5947cc5da3a4c26a7943021dbae6a46ee9e
liw <wyx_msn@hotmail.com>
2018-02-07 19:51:38 +0800

    init

---
+++ src/main/java/com/wuzhi/sprout/SproutApplication.java
@@ -0,0 +1,12 @@
+package com.wuzhi.sprout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SproutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SproutApplication.class, args);
+	}
+}
