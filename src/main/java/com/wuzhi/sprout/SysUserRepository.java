fbcbfd598d2f5993978a4409adfab4d2f4a8496a
liw <wyx_msn@hotmail.com>
2018-02-11 21:45:07 +0800

    simple

---
+++ src/main/java/com/wuzhi/sprout/SysUserRepository.java
@@ -0,0 +1,7 @@
+package com.wuzhi.sprout;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SysUserRepository extends JpaRepository<SysUser, Long> {
+    SysUser findByUsername(String username);
+}
